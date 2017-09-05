077774a901e1c81ed8b575d5748c200456574e27
Peter Thompson <peter@myaxis.co.uk>
2017-09-05 10:10:58 +0100

    testing branching with git hub

--- util.js
+++ util.js
@@ -3,7 +3,16 @@ const config = {
     vURL: 'http://x:M60yC87vmul6PPp3fqw7RFZ9shcE8zRO@35.197.193.32:48445',
     vendorAddress: 'RMXF9JgfMaLSCpHyugaLahcn2TkqJKvh9A',
     mailFromAddress: 'kpcoin@myaxis.co.uk',
-    mailFromPwd: 'qIGznKkwG03nqUhrUFvF3FV7zc2C1WFL'
+    mailFromPwd: 'qIGznKkwG03nqUhrUFvF3FV7zc2C1WFL',
+    configDB: {
+        user: 'KPcoinSQLUser',
+        password: 'myDBpass123',
+        server: 'MYAXISPC004\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
+        database: 'KPcoin',
+        options: {
+        encrypt: false // Use this if you're on Windows Azure 
+        }
+    }
 };
 
 module.exports.config = config;
