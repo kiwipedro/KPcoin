462a058bfa54a079cd3c4132d78ef9ea7665a397
Peter Thompson <peter@myaxis.co.uk>
2017-09-12 17:36:25 +0100

    take two

--- util.js
+++ util.js
@@ -7,30 +7,30 @@ const config = {
     mailFromPwd: 'qIGznKkwG03nqUhrUFvF3FV7zc2C1WFL'
 };
 
-const configDB = {
-        user: 'KPcoinSQLUser',
-        password: 'myDBpass123',
-        server: 'MYAXISPC004\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
-        database: 'KPcoin',
-        options: {
-        encrypt: false // Use this if you're on Windows Azure 
-        }
-    };
+//const configDB = {
+//        user: 'KPcoinSQLUser',
+//        password: 'myDBpass123',
+//        server: 'MYAXISPC004\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
+//        database: 'KPcoin',
+//        options: {
+//        encrypt: false // Use this if you're on Windows Azure 
+//        }
+//    };
 
 
 
-//var configDB = {
-//    user: 'kpc.sql',
-//    password: 'jaEeCekeQa7PHP7ur3rMvneRcptDEoZNo',
-//    server: 'az-kpc-db-sv1.database.windows.net', 
-//    database: 'KPcoin',
-//    encrypt: true,
-//    connectionTimeout: 50000,
-//   // port: 1433,
-//    options: {
-//        encrypt: true // Use this if you're on Windows Azure 
-//    }
-//};
+var configDB = {
+    user: 'kpc.sql',
+    password: 'jaEeCekeQa7PHP7ur3rMvneRcptDEoZNo',
+    server: 'az-kpc-db-sv1.database.windows.net', 
+    database: 'KPcoin',
+    encrypt: true,
+    connectionTimeout: 50000,
+   // port: 1433,
+    options: {
+        encrypt: true // Use this if you're on Windows Azure 
+    }
+};
 
 
 module.exports.config = config;
